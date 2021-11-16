16ad5ebce746e45a96f9e1c9245077c8aa99cd6b
Ivan Mc Kinley <2631311+ivanthelad@users.noreply.github.com>
2021-11-16 23:33:38 +0000

    Stripped Cosmos metadata from response

--- GetRating/index.js
+++ GetRating/index.js
@@ -1,3 +1,4 @@
+const response_def = ['userId', 'productId', 'locationName', 'rating', 'userNotes']
 module.exports = async function (context, req, rating) {
     context.log('GetRating called ');
     
@@ -9,19 +10,26 @@ module.exports = async function (context, req, rating) {
             let notfound = {
                 error: 'NOT_FOUND',
                 message: "Rating with id not found"+ratingid
-
               };
             context.res = {
                 status: 404,
-                body: JSON.stringify(notfound)
+                body: JSON.stringify(notfound,null,2)
             };
         }
         else
         {
             context.log("Found Rating item, Description=" + rating);
+            let response = {
+                id: rating.id,
+                userId: rating.userId,
+                productId: rating.productId,
+                locationName: rating.locationName,
+                rating: rating.rating,
+                userNotes: rating.userNotes,
+              };
             context.res = {
                 status: 200,
-                body: rating
+                body: JSON.stringify(response,null,2)
             };
         }  
     } catch (e) {
@@ -31,7 +39,7 @@ module.exports = async function (context, req, rating) {
           };
         context.res = {
             status: 500,
-            body: JSON.stringify(error)
+            body: JSON.stringify(error,null,2)
         };
      
     }
