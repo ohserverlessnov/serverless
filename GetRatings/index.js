e7bef88857226d1a20738d37cde20ed95d73ae07
codeHysteria28 <graphicglobal67@gmail.com>
2021-11-16 15:05:13 +0000

    changes for getRatings API

--- GetRatings/index.js
+++ GetRatings/index.js
@@ -1,13 +1,24 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const userID = req.query.userId;
+    const documents = context.bindings.ratings;
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    if(userID){
+        documents.map(document => {
+            if(document.userId === userID){
+                return document;
+            }
+        });
+
+        context.res = {
+            body: documents
+        };
+    }else {
+        context.res = {
+            body: `No details found for given userId: ${userID} !`
+        }
+    }
+
+    context.done();
 }
