56f7ae3ca61c2f8dc34bbad879d2c0e431bde508
khowling <khowling@gmail.com>
2021-11-16 16:23:55 +0000

    add guid

--- CreateRating/index.js
+++ CreateRating/index.js
@@ -58,7 +58,11 @@ module.exports = async function (context, req) {
 
         context.bindings.ratingDocument = JSON.stringify({
             ...body,
-            timestamp: new Date,
+            timestamp: new Date(),
+            id: 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {  
+                var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);  
+                return v.toString(16);  
+             })
         });
     
     
