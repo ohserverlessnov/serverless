b10b6449b1527cb1069f97e0ae330248784d5e72
Ivan Mc Kinley <2631311+ivanthelad@users.noreply.github.com>
2021-11-18 14:02:55 +0000

    Lab8 pushing to SB based on receiptURL

--- SalesEventProcessing/index.js
+++ SalesEventProcessing/index.js
@@ -1,16 +1,31 @@
 module.exports = async function (context, eventHubMessages) {
     context.log(`JavaScript eventhub trigger function called for message array ${eventHubMessages}`);
-    
+
     // go through every message
     context.bindings.posData = [];
+    context.bindings.posDataSB = [];
     eventHubMessages.forEach((salesItem, index) => {
         context.log(`Processed Item ${index}\n${JSON.stringify(salesItem)}`);
-        
+
         // Convert each sales item to a document, indexed by the salesNumber
         context.bindings.posData.push({
             ...salesItem,
             id: salesItem.header.salesNumber
         });
+        // Convert each sales item to a document, indexed by the salesNumber
+        if( salesItem.header.receiptUrl ){
+            context.bindings.posDataSB.push({
+                "totalItems": salesItem.details.length,
+                "totalCost": Number(salesItem.header.totalCost),
+                "salesNumber": salesItem.header.salesNumber,
+                "salesDate": salesItem.header.salesDate,
+                "storeLocation": salesItem.header.locationId,
+                "receiptUrl": salesItem.header.receiptUrl 
+            });
+        }
+
     });
+ //   [salesItem.header.locationName].filter(i => !!i).join(',')
+ 
     context.done();
 };
